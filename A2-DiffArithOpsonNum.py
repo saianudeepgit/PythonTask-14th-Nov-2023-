aacbaadacb5027f4a8e0113ff833bb1947bb2da4
saianudeepgit <140361870+saianudeepgit@users.noreply.github.com>
2023-11-15 09:19:45 +0530

    program to perform different Arithmetic Operations on numbers in Python

---
+++ A2-DiffArithOpsonNum.py
@@ -0,0 +1,23 @@
+num1 = int(10)
+num2 = int(40)
+'""addition'""
+add = num1 + num2
+'""subraction'""
+sub = num1 - num2
+'""multiplication'""
+mul = num1 * num2
+'""Division'""
+div = num1 / num2
+'""floor division'""
+floor_div = num1 // num2
+'""Exponent'""
+power = num1 ** num2
+'""modules'""
+modulus = num1 % num2
+print('Sum of ',num1+num2,'is')
+print('Sub of ',num1-num2,'is')
+print('Product of',num1*num2,'is')
+print('Division of ',num1/num2,'is')
+print('Floor Division of' ,num1//num2,'is')
+print('Exponent of ',num1**num2,'is')
+print('Modulus of ',num1%num2,'is')
