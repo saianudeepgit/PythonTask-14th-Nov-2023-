781247d79092d4d6307b3826a0995d8b70a825a3
saianudeepgit <140361870+saianudeepgit@users.noreply.github.com>
2023-11-15 09:22:55 +0530

    program to create, concatenate and print a string and accessing sub-string from a given string

---
+++ A3-CreateConcatSubStr.py
@@ -0,0 +1,14 @@
+# Create a string
+string = "My Hobbies Singing"
+print("String:", string)
+
+# Concatenate strings
+additional_text = " Developer"
+concatenated_string = string + additional_text
+print("Concatenated String:", concatenated_string)
+
+# Access substring
+substring_start = 0
+substring_end = 5
+substring = string[substring_start:substring_end]
+print("Substring:", substring)
