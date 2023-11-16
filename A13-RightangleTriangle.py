10a3d771c32514ee1f80f758bf602ebcca7440a0
saianudeepgit <140361870+saianudeepgit@users.noreply.github.com>
2023-11-16 23:46:38 +0530

    Program to indicate Rightangle for given sides

---
+++ A13-RightangleTriangle.py
@@ -0,0 +1,7 @@
+leng = float(input("Enter the length:"))
+heig = float(input("Enter the height:"))
+hypo = float(input("Enter the hypotaneous:"))
+if hypo**2 == ((leng**2) + (heig**2)):
+  print("It's a right triangle!!!")
+else:
+  print("It's not a right triangle")
